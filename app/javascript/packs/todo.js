536363521fee491bc3f643246f3494c682c8e821
koda <hogehogehoge@gmail.com>
2020-01-03 16:42:33 +0900

    navbarでメニュー表示機能実装

--- app/javascript/packs/todo.js
+++ app/javascript/packs/todo.js
@@ -1,5 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
+import Header from './components/header.vue'
 
-var app = New Vue({
+var app = new Vue({
   el: '#app',
+  components: {
+    'navbar': Header,
+  }
 });
