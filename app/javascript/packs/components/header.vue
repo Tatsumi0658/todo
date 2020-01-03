0338ca32a407ca6a350a912951ef6095c4a5abe4
koda <hogehogehoge@gmail.com>
2020-01-03 16:32:34 +0900

    headerのcomponentの実装

---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul id="dropdown" class="dropdown-content">
+      <li>
+        <a href="#">Top</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+    <nav>
+      <div class="nav-wrapper container">
+        <a href="/" class="brand-logo left">Todo Application</a>
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <a href="#">Top</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
---
+++ app/javascript/packs/todo.js
@@ -0,0 +1,5 @@
+import Vue from 'vue/dist/vue.esm'
+
+var app = New Vue({
+  el: '#app',
+});
